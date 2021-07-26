377c54ad0627721fa0065e9b82cdd1c8e2a4f13f
ryougiii <61556798+ryougiii@users.noreply.github.com>
2021-07-26 20:22:43 +0800

    u

--- Tracing/hittable_list.h
+++ Tracing/hittable_list.h
@@ -61,4 +61,70 @@ public:
     std::vector<shared_ptr<hittable>> objects;
 };
 
+class flip_face : public hittable
+{
+public:
+    flip_face(shared_ptr<hittable> p) : ptr(p) {}
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+    {
+        if (!ptr->hit(r, t_min, t_max, rec))
+            return false;
+
+        rec.front_face = !rec.front_face;
+        return true;
+    }
+
+    virtual bool bounding_box(double t0, double t1, aabb &output_box) const
+    {
+        return ptr->bounding_box(t0, t1, output_box);
+    }
+
+public:
+    shared_ptr<hittable> ptr;
+};
+
+class box : public hittable
+{
+public:
+    box() {}
+    box(const vec3 &p0, const vec3 &p1, shared_ptr<material> ptr);
+
+    virtual bool hit(const ray &r, double t0, double t1, hit_record &rec) const;
+
+    virtual bool bounding_box(double t0, double t1, aabb &output_box) const
+    {
+        output_box = aabb(box_min, box_max);
+        return true;
+    }
+
+public:
+    vec3 box_min;
+    vec3 box_max;
+    hittable_list sides;
+};
+
+box::box(const vec3 &p0, const vec3 &p1, shared_ptr<material> ptr)
+{
+    box_min = p0;
+    box_max = p1;
+
+    sides.add(make_shared<xy_rect>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
+    sides.add(make_shared<flip_face>(
+        make_shared<xy_rect>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr)));
+
+    sides.add(make_shared<xz_rect>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
+    sides.add(make_shared<flip_face>(
+        make_shared<xz_rect>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr)));
+
+    sides.add(make_shared<yz_rect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+    sides.add(make_shared<flip_face>(
+        make_shared<yz_rect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr)));
+}
+
+bool box::hit(const ray &r, double t0, double t1, hit_record &rec) const
+{
+    return sides.hit(r, t0, t1, rec);
+}
+
 #endif
--- Tracing/tracing.cpp
+++ Tracing/tracing.cpp
@@ -32,7 +32,7 @@ vec3 ray_color(const ray &r, const vec3 &background, const hittable &world, int 
     ray scattered;
     vec3 attenuation;
     vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
-    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered)) //如果返回false认为被吸收
         return emitted;
 
     return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
@@ -120,10 +120,31 @@ hittable_list simple_light()
     objects.add(make_shared<sphere>(vec3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
     objects.add(make_shared<sphere>(vec3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
-    auto difflight = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(4, 4, 4)));
+    auto difflight = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(1, 1, 1))); //值越大反射光线最后加光源emit值平均后越大
     objects.add(make_shared<sphere>(vec3(0, 7, 0), 2, difflight));
     objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
 
+    return objects;
+}
+
+hittable_list cornell_box()
+{
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(15, 15, 15)));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+
+    objects.add(make_shared<box>(vec3(130, 0, 65), vec3(295, 165, 230), white));
+    objects.add(make_shared<box>(vec3(265, 0, 295), vec3(430, 330, 460), white));
     return objects;
 }
 
@@ -134,9 +155,9 @@ int main()
     ofstream ou;
     ou.open(strtx);
     //ou.open(strho);
-    const int image_width = 400;
-    const int image_height = 200;
-    const int samples_per_pixel = 100;
+    const int image_width = 100;
+    const int image_height = 100;
+    const int samples_per_pixel = 10000;
     const int max_depth = 20;
     const vec3 background(0, 0, 0);
 
@@ -144,18 +165,19 @@ int main()
        << image_width << " " << image_height << "\n255\n";
 
     //auto world = random_scene();
-    auto world = simple_light();
+    auto world = cornell_box();
     //auto world = two_perlin_spheres();
 
     const auto aspect_ratio = double(image_width) / image_height;
 
-    vec3 lookfrom(13, 2, 3);
-    vec3 lookat(0, 0, 0);
+    vec3 lookfrom(278, 278, -800);
+    vec3 lookat(278, 278, 0);
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
     auto aperture = 0.0;
+    auto vfov = 40.0;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     for (int j = image_height - 1; j >= 0; --j)
     {
